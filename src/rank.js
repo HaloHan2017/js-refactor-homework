36651334fce489bcc7d7c87455b2f918022decf9
HaloHan2017 <2332722908@qq.com>
2020-08-28 17:58:54 +0800

    [js-refactor-homework] [jerry] refactor : reduce one if-condition

--- src/rank.js
+++ src/rank.js
@@ -74,10 +74,7 @@ function handleResultWithZone(result,history,voyage){
 
 function voyageProfitFactor (voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china') {
-    result += 1;
-  }
-  if (voyage.zone === 'east-indies') {
+  if (voyage.zone === 'china' || voyage.zone === 'east-indies') {
     result += 1;
   }
   return handleResultWithZone(result,history,voyage);
